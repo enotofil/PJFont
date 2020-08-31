9d76d81bae38c9b7b6634746315846621ba8016d
RUSshy <ismaalove@gmail.com>
2020-08-31 23:43:23 +0200

    Make window resizable

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"github.com/enotofil/PJFont/util"
 )
 
+type Game struct{}
 var (
 	ui *util.FontUI
 )
@@ -18,23 +19,32 @@ func init() {
 	ui = util.NewFontUI()
 }
 
-func update(screen *ebiten.Image) error {
+
+func (g *Game) Update(screen *ebiten.Image) error {
+	return nil
+}
+
+
+func (g *Game) Draw(screen *ebiten.Image) {
 	if ebiten.IsKeyPressed(ebiten.KeyEscape) {
 		os.Exit(0)
-	}
-	if ebiten.IsDrawingSkipped() {
-		return nil
 	}
 	screen.Fill(color.NRGBA{0x2F, 0x3F, 0x5F, 0xff})
 
 	ui.Update(screen)
 
-	return nil
+	return
 }
-
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return util.WindowW, util.WindowH
+}
 func main() {
-	// ebiten rules
-	if err := ebiten.Run(update, util.WindowW, util.WindowH, 1, "PJFont"); err != nil {
+		
+	ebiten.SetWindowSize(util.WindowW, util.WindowH)
+	ebiten.SetWindowTitle("PJFont")
+	ebiten.SetWindowResizable(true)
+
+	if err := ebiten.RunGame(&Game{}); err != nil {
 		log.Fatal(err)
 	}
 }
