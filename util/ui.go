d6ba394af623eb9936a007c2485b6dd178aabc69
Fil <enotofil@yandex.ru>
2020-09-04 00:13:23 +0300

    UI resize function

--- util/ui.go
+++ util/ui.go
@@ -20,14 +20,9 @@ import (
 
 // TODO multiple ttf folders, sort fonts in alphabet order
 const (
-	//WindowW : app screen width
-	WindowW = 1280
-	//WindowH : app screen height
-	WindowH    = 720
-	paramW     = 260 //384
-	paramH     = 28
-	previewW   = 512
-	previewH   = 512
+	paramWidth  = 260 // parameters column width
+	paramHeight = 28  // one string height
+
 	uiFontSize = 14
 
 	outDir       = "out"
@@ -37,6 +32,18 @@ const (
 )
 
 var (
+	//WindowWidth : app screen width
+	WindowWidth    = 1280
+	minWindowWidth = 1024
+	//WindowHeight : app screen height
+	WindowHeight    = 720
+	minWindowHeight = 600
+
+	packedFontX   = float64(paramWidth)
+	packedFontY   = 0.0
+	previewWidth  = 512
+	previewHeight = 512
+
 	sampleStrings = []string{}
 	helpText      = "ARROWS: select and change  |  HOME/END: min/max value  |  " +
 		"ENTER: apply  |  C: change background  |  S: save "
@@ -68,22 +75,22 @@ func NewFontUI() *FontUI {
 	uiFont := loader.getFace(0, uiFontSize)
 	params := []fontParam{
 		// name, value, min, max, step
-		fontParam{"font №", 0, 0, len(loader.fontNames) - 1, 1},            // 0
-		fontParam{"font size, px", 24, 8, 56, 1},                           // 1
-		fontParam{"top gradient, %", 100, 50, 100, 5},                      // 2
-		fontParam{"mid gradient, %", 100, 50, 100, 5},                      // 3
-		fontParam{"bottom gradient, %", 70, 50, 100, 5},                    // 4
-		fontParam{"outline width, px", 1, 0, 3, 1},                         // 5
-		fontParam{"x shadow dist, px", 1, 0, 4, 1},                         // 6
-		fontParam{"y shadow dist, px", 1, 0, 4, 1},                         // 7
-		fontParam{"shadow blur", 2, 0, 4, 1},                               // 8
-		fontParam{"add spacing, px", 1, 0, 10, 1},                          // 9
-		fontParam{"add line height, px", 1, 0, 10, 1},                      // 10
-		fontParam{"bitmap width, px", previewW, sizeStep, 1024, sizeStep},  // 11
-		fontParam{"bitmap height, px", previewH, sizeStep, 1024, sizeStep}, // 12
+		{"font №", 0, 0, len(loader.fontNames) - 1, 1},                 // 0
+		{"font size, px", 24, 8, 56, 1},                                // 1
+		{"top gradient, %", 100, 50, 100, 5},                           // 2
+		{"mid gradient, %", 100, 50, 100, 5},                           // 3
+		{"bottom gradient, %", 70, 50, 100, 5},                         // 4
+		{"outline width, px", 1, 0, 3, 1},                              // 5
+		{"x shadow dist, px", 1, 0, 4, 1},                              // 6
+		{"y shadow dist, px", 1, 0, 4, 1},                              // 7
+		{"shadow blur", 2, 0, 4, 1},                                    // 8
+		{"add spacing, px", 1, 0, 10, 1},                               // 9
+		{"add line height, px", 1, 0, 10, 1},                           // 10
+		{"bitmap width, px", previewWidth, sizeStep, 1024, sizeStep},   // 11
+		{"bitmap height, px", previewHeight, sizeStep, 1024, sizeStep}, // 12
 	}
 
-	paramImg, _ := ebiten.NewImage(paramW, paramH, ebiten.FilterDefault)
+	paramImg, _ := ebiten.NewImage(paramWidth, paramHeight, ebiten.FilterDefault)
 	paramImg.Fill(selectColor)
 
 	return &FontUI{
@@ -150,23 +157,34 @@ func (ui *FontUI) Update(screen *ebiten.Image) bool {
 	}
 
 	// Draw packed preview background
+	ebitenutil.DrawRect(
+		screen,
+		paramWidth,
+		0,
+		float64(previewWidth),
+		float64(previewHeight),
+		color.Black, // RGBA{bgColor, bgColor, 0x00, 0xFF},
+	)
 	op := &ebiten.DrawImageOptions{}
 	op.GeoM.Scale(previewScale*bgSquareSize, previewScale*bgSquareSize)
-	op.GeoM.Translate(paramW, 0.0)
+	op.GeoM.Translate(packedFontX, packedFontY)
 	screen.DrawImage(ui.bgImg, op)
 
 	// Draw packed font preview
 	op = &ebiten.DrawImageOptions{}
 	op.GeoM.Scale(previewScale, previewScale)
-	op.GeoM.Translate(paramW, 0.0)
+	op.GeoM.Translate(packedFontX, packedFontY) // paramW, 0.0)
 	screen.DrawImage(ui.packedImg, op)
+
+	// Draw preview scale info
+	text.Draw(screen, fmt.Sprintf("scale\n%.2f", previewScale), ui.uiFont, paramWidth+2, 12, color.White)
 
 	// Draw parameters, highlight selected
 	op = &ebiten.DrawImageOptions{}
-	op.GeoM.Translate(0.0, paramH*float64(ui.curParam))
+	op.GeoM.Translate(0.0, paramHeight*float64(ui.curParam))
 	screen.DrawImage(ui.paramImg, op)
 	for i, p := range ui.params {
-		y := paramH * (i + 1)
+		y := paramHeight * (i + 1)
 		if i == ui.curParam {
 			text.Draw(screen, p.toString(), ui.uiFont, 1, y-8, yellowColor)
 		} else {
@@ -175,21 +193,21 @@ func (ui *FontUI) Update(screen *ebiten.Image) bool {
 	}
 
 	// Draw /fonts.ttf content, highlight current
-	fontsOnPage := previewH / paramH
+	fontsOnPage := previewHeight / paramHeight
 	page := ui.params[0].value / fontsOnPage
 	posOnPage := ui.params[0].value % fontsOnPage
 
 	op = &ebiten.DrawImageOptions{}
-	op.GeoM.Scale(2, 1)
-	op.GeoM.Translate(paramW+previewW, float64(paramH*posOnPage))
+	op.GeoM.Scale(3, 1)
+	op.GeoM.Translate(float64(paramWidth+previewWidth), float64(paramHeight*posOnPage))
 	screen.DrawImage(ui.paramImg, op)
 
-	y := paramH
+	y := paramHeight
 	fontN := page * fontsOnPage
-	for y < previewH && fontN < len(ui.loader.fontNames) {
+	for y < previewHeight && fontN < len(ui.loader.fontNames) {
 		f := ui.loader.fontNames[fontN]
-		text.Draw(screen, fmt.Sprintf("%2d. %s", fontN, f), ui.uiFont, paramW+previewW+10, y-9, color.White)
-		y += paramH
+		text.Draw(screen, fmt.Sprintf("%2d. %s", fontN, f), ui.uiFont, paramWidth+previewWidth+10, y-9, color.White)
+		y += paramHeight
 		fontN++
 	}
 
@@ -197,30 +215,30 @@ func (ui *FontUI) Update(screen *ebiten.Image) bool {
 	ebitenutil.DrawRect(
 		screen,
 		0.0,
-		previewH+1,
-		float64(paramW+previewW)-1,
-		float64(WindowH-previewH-uiFontSize*2),
+		float64(previewHeight)+1,
+		float64(paramWidth+previewWidth)-1,
+		float64(WindowHeight-previewHeight-uiFontSize*2),
 		color.RGBA{bgColor, bgColor, bgColor, 0xFF},
 	)
 	for i, s := range ui.config.SampleStrings {
-		ui.drawString(screen, s, 8, previewH+ui.font.LineHeight*(i+1), 1)
+		ui.drawString(screen, s, 8, previewHeight+ui.font.LineHeight*(i+1), 1)
 	}
 
 	// Draw x2 sample string and background
 	ebitenutil.DrawRect(
 		screen,
-		float64(paramW+previewW),
-		previewH+1,
-		float64(WindowW-paramW-previewW),
-		float64(WindowH-previewH-uiFontSize*2),
+		float64(paramWidth+previewWidth),
+		float64(previewHeight)+1,
+		float64(WindowWidth-paramWidth-previewWidth),
+		float64(WindowHeight-previewHeight-uiFontSize*2),
 		color.RGBA{bgColor, bgColor, bgColor, 0xFF},
 	)
-	ui.drawString(screen, ui.font.Name, paramW+previewW+8, previewH+ui.font.LineHeight*2+8, 2)
+	ui.drawString(screen, ui.font.Name, paramWidth+previewWidth+8, previewHeight+ui.font.LineHeight*2+8, 2)
 
 	// Draw help string
-	text.Draw(screen, helpText+ui.font.Name+".*", ui.uiFont, 16, WindowH-uiFontSize/2, color.White)
+	text.Draw(screen, helpText+ui.font.Name+".*", ui.uiFont, 16, WindowHeight-uiFontSize/2, color.White)
 	// Draw saved indicator
-	text.Draw(screen, statusText, ui.uiFont, WindowW-120, WindowH-uiFontSize/2, yellowColor)
+	text.Draw(screen, statusText, ui.uiFont, WindowWidth-120, WindowHeight-uiFontSize/2, yellowColor)
 
 	return false
 }
@@ -271,13 +289,14 @@ func (ui *FontUI) submit() {
 	)
 
 	ui.packedImg, _ = ebiten.NewImageFromImage(ui.font.img, ebiten.FilterDefault)
+
 	imgW := ui.packedImg.Bounds().Dx()
 	imgH := ui.packedImg.Bounds().Dy()
-	previewScale = math.Min(
-		float64(previewW)/float64(imgW),
-		float64(previewH)/float64(imgH),
-	)
+
+	ui.Resize(WindowWidth, WindowHeight)
+
 	ui.bgImg, _ = ebiten.NewImage(imgW/bgSquareSize, imgH/bgSquareSize, ebiten.FilterDefault)
+
 	if ui.font.doesntFit() {
 		ui.bgImg.Fill(color.NRGBA{192, 64, 64, 255})
 	} else {
@@ -303,5 +322,37 @@ func (ui *FontUI) drawString(screen *ebiten.Image, s string, x, y, scale int) {
 		op.GeoM.Translate(float64(x+gl.XOff*scale), float64(y+gl.YOff*scale))
 		screen.DrawImage(ui.packedImg.SubImage(src).(*ebiten.Image), op)
 		x += gl.XAdv * scale
+	}
+}
+
+// Resize UI for new window size
+func (ui *FontUI) Resize(newWidth, newHeight int) {
+
+	if newWidth < minWindowWidth {
+		WindowWidth = minWindowWidth
+	} else {
+		WindowWidth = newWidth
+	}
+
+	if newHeight < minWindowHeight {
+		WindowHeight = minWindowHeight
+	} else {
+		WindowHeight = newHeight
+	}
+
+	previewWidth = int(float64(WindowWidth) * 0.54)
+	previewHeight = int(float64(WindowHeight) * 0.78)
+
+	if ui.packedImg != nil {
+		imgW := ui.packedImg.Bounds().Dx()
+		imgH := ui.packedImg.Bounds().Dy()
+		previewScale = math.Min(
+			float64(previewWidth)/float64(imgW),
+			float64(previewHeight)/float64(imgH),
+		)
+		previewScale = math.Floor(previewScale*4) / 4
+
+		packedFontX = paramWidth + (float64(previewWidth)-float64(imgW)*previewScale)/2
+		packedFontY = (float64(previewHeight) - float64(imgH)*previewScale) / 2
 	}
 }
